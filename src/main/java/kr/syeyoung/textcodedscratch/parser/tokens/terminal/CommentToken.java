4fad3ceb1f2eb7b90eff5bb36827a250852051ea
syeyoung <cyoung06@naver.com>
2020-07-12 12:35:09 +0900

    Comments are kinda statement

--- src/main/java/kr/syeyoung/textcodedscratch/parser/tokens/terminal/CommentToken.java
+++ src/main/java/kr/syeyoung/textcodedscratch/parser/tokens/terminal/CommentToken.java
@@ -1,8 +1,10 @@
 package kr.syeyoung.textcodedscratch.parser.tokens.terminal;
 
 import kr.syeyoung.textcodedscratch.parser.ParserNode;
+import kr.syeyoung.textcodedscratch.parser.tokens.nonterminal.statements.Statements;
+import kr.syeyoung.textcodedscratch.parser.util.ScriptBuilder;
 
-public class CommentToken implements TerminalNode {
+public class CommentToken implements TerminalNode, Statements {
     private String match;
     public CommentToken(String matched) {
         this.match = matched;
@@ -12,9 +14,33 @@ public class CommentToken implements TerminalNode {
         return match;
     }
 
-
     @Override
     public ParserNode[] getChildren() {
         return new ParserNode[0];
+    }
+
+
+    @Override
+    public int getStackCountAtExecution() {
+        return stackAtExe;
+    }
+
+    @Override
+    public Object[] buildJSON(String parentId, String nextId, ScriptBuilder builder) {
+        return null;
+    }
+
+    private int stack;
+    private int stackAtExe = -1;
+    @Override
+    public void setCurrentStack(int stackSize) {
+        this.stack = stackSize;
+        if (stackAtExe == -1)
+            stackAtExe = stackSize;
+    }
+
+    @Override
+    public int getCurrentStack() {
+        return stack;
     }
 }
